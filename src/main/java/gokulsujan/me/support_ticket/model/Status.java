e803acde181ac96550b60c6c0144020ab4baadf4
Gokul Sujan <gokul.sujan@outlook.com>
2024-08-20 14:31:14 +0530

    Created support ticket model

---
+++ src/main/java/gokulsujan/me/support_ticket/model/Status.java
@@ -0,0 +1,8 @@
+package gokulsujan.me.support_ticket.model;
+
+public enum Status {
+    REGISTERED,
+    ASSIGNED,
+    PAUSED,
+    CLOSED
+}
---
+++ src/main/java/gokulsujan/me/support_ticket/model/SupportTicket.java
@@ -0,0 +1,49 @@
+package gokulsujan.me.support_ticket.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SupportTicket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String query;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
